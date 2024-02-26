a6b1106a6adfd428f970444ce00bee44e9b95847
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-26 19:10:54 +0100

    added styling and logic

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,20 +2,21 @@ import React, { useState } from 'react'
 import Genorator from './hooks/Genorator.jsx'
 const Home = () => {
   const [newText, setNewText] = useState()
+  const [textCopied, setTextCopied] = useState()
   const [loremCount, setLoremCount] = useState(1)
 
   const handleCopyText = async () => {
     try {
       await navigator.clipboard.writeText(newText)
-      setNewText('TEXT HAS BEEN COPIED')
+      setTextCopied('COPIED!')
     } catch (err) {
-      setNewText('Failed to copy: ', err)
+      setTextCopied('Failed... copy: ', err)
     }
   }
   return (
     <>
       <div className="flex flex-col h-[100vh] w-[100vw] text-center items-center justify-end">
-        <div className="h-[10%] fixed top-0 left-0 w-[100%] text-center items-center justify-evenly flex flex-row bg-zinc-400 text-white border-b-2 border-white">
+        <div className="h-[10%] fixed top-0 left-0 w-[100%] text-center items-center justify-center gap-4 flex flex-row bg-zinc-400 text-white border-b-2 border-white">
           <input
             className="text-black rounded-[6px]"
             type="text"
@@ -34,8 +35,9 @@ const Home = () => {
             get lorems
           </button>
           <button onClick={handleCopyText}>copy text</button>
+          <p>{textCopied}</p>
         </div>
-        <div className="h-[90%] w-[100%] overflow-auto text-center items-center justify-evenly flex flex-row bg-zinc-400 text-white">
+        <div className="h-[90%] w-[100%] overflow-auto text-start items-start  flex flex-row bg-zinc-400 text-white p-4">
           <p>{newText}</p>
         </div>
       </div>
