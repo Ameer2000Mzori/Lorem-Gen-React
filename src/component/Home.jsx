f26836882145e500d7d232ad112f9bbbe6bac00e
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-26 19:08:13 +0100

    styled to text and everything

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -14,24 +14,31 @@ const Home = () => {
   }
   return (
     <>
-      <input
-        type="text"
-        value={loremCount}
-        onChange={(e) => {
-          setLoremCount(e.target.value)
-        }}
-      />
-      <button
-        onClick={() => {
-          const newLorem = Genorator(loremCount)
-          console.log(newLorem, 'in home component')
-          setNewText(newLorem)
-        }}
-      >
-        get lorems
-      </button>
-      <button onClick={handleCopyText}>copy text</button>
-      <p>{newText}</p>
+      <div className="flex flex-col h-[100vh] w-[100vw] text-center items-center justify-end">
+        <div className="h-[10%] fixed top-0 left-0 w-[100%] text-center items-center justify-evenly flex flex-row bg-zinc-400 text-white border-b-2 border-white">
+          <input
+            className="text-black rounded-[6px]"
+            type="text"
+            value={loremCount}
+            onChange={(e) => {
+              setLoremCount(e.target.value)
+            }}
+          />
+          <button
+            onClick={() => {
+              const newLorem = Genorator(loremCount)
+              console.log(newLorem, 'in home component')
+              setNewText(newLorem)
+            }}
+          >
+            get lorems
+          </button>
+          <button onClick={handleCopyText}>copy text</button>
+        </div>
+        <div className="h-[90%] w-[100%] overflow-auto text-center items-center justify-evenly flex flex-row bg-zinc-400 text-white">
+          <p>{newText}</p>
+        </div>
+      </div>
     </>
   )
 }
