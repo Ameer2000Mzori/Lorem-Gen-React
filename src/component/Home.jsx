d156c7977ddc38c0c9b324d4ec0da0a091a74993
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-26 17:52:44 +0100

    added lorem package

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Genorator from './hooks/Genorator.jsx'
 const Home = () => {
+  Genorator()
   return <div>Home</div>
 }
 
--- src/component/hooks/Genorator.jsx
+++ src/component/hooks/Genorator.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Genorator = () => {
-  return <div>Genorator</div>
-}
+  const [data, setData] = []
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const numberOfParagraphs = 4
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await axios.get(
+          ` https://api.api-ninjas.com/v1/loremipsum?paragraphs=${numberOfParagraphs}`,
+          { headers: { 'X-Api-Key': 'YOUR_API_KEY' } }
+        )
 
+        console.log(res.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getData()
+  })
+  return { data, error, loading }
+}
+// https://api.api-ninjas.com/v1/loremipsum?paragraphs=
 export default Genorator
