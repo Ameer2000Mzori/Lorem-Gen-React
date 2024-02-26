d6adc784b8f38cd41d776d88b1948fd8ef3c1ea1
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-26 17:43:49 +0100

    added files / connected them

---
+++ src/component/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import Genorator from './hooks/Genorator.jsx'
+const Home = () => {
+  return <div>Home</div>
+}
+
+export default Home
---
+++ src/component/hooks/Genorator.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Genorator = () => {
+  return <div>Genorator</div>
+}
+
+export default Genorator
